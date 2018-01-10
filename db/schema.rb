2ee6749f400ed5e45cf85ff98c38c4c98ed8b439
shoki <ac.milan.22x36@gmail.com>
2018-01-10 12:29:08 +0900

    migrate ckeditor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110031235) do
+ActiveRecord::Schema.define(version: 20180110122249) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",                  null: false
@@ -20,6 +20,18 @@ ActiveRecord::Schema.define(version: 20180110031235) do
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string   "data_file_name",               null: false
+    t.string   "data_content_type"
+    t.integer  "data_file_size"
+    t.string   "type",              limit: 30
+    t.integer  "width"
+    t.integer  "height"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
   end
 
   create_table "users", force: :cascade do |t|
