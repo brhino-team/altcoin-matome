c83bb790bb73bda2aa576e23f23d10f09bf05a51
shoki <ac.milan.22x36@gmail.com>
2018-01-10 15:29:55 +0900

    image_uploader

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ApplicationRecord
   belongs_to :user
   has_many :images
-  validates :title, :body, presence: true
+  validates :title, :body, :thum, presence: true
+  mount_uploader :thum, ImageUploader
 end
