fa762117f1bda9df2ca335b9a093dffc5baf379d
shoki <ac.milan.22x36@gmail.com>
2018-01-10 14:28:48 +0900

    write model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :user
+  has_many :images
+  validates :title, :body, presence: true
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,5 @@
 class Image < ApplicationRecord
+  belongs_to :article
+  mount_uploader :image, ImageUploader
+  validates :image_url, presence: true
 end
