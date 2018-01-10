ef3333593ceacf6e9f44621134d7dcfa554c1a9a
shoki <ac.milan.22x36@gmail.com>
2018-01-10 16:23:48 +0900

    fix thum

--- db/migrate/20180110122250_add_thum_to_articles.rb
+++
@@ -1,6 +0,0 @@
-class AddThumToArticles < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :articles, :thun, :string
-    add_column :articles, :thum, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110122250) do
+ActiveRecord::Schema.define(version: 20180110122249) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",                  null: false
     t.text     "body",                   null: false
     t.integer  "pv",         default: 0
+    t.string   "thum",                   null: false
     t.integer  "user_id"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
-    t.string   "thum"
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
