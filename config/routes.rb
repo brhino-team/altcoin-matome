3da2bc76c3da5dac5f2b63c8e1ad964611af8237
shoki <ac.milan.22x36@gmail.com>
2018-01-11 12:01:00 +0900

    write routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
   root 'matomes#index'
-  resources :articles, only: %i(new create show)
+  resources :articles, only: %i(new create show edit update)
 end
