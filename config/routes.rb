5a6de04438bf1743a0e7cb8a8802fc7e4b44138a
shoki <ac.milan.22x36@gmail.com>
2018-01-11 14:31:52 +0900

    add destroy_confirmation

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :users
   root 'matomes#index'
-  resources :articles, only: %i(new create show edit update destroy)
+  resources :articles, only: %i(new create show edit update destroy) do
+    member do
+      get :destroy_confirmation
+    end
+  end
 end
