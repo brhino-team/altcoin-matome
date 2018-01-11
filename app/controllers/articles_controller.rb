aacce7db91448d40caabd85a4fc00b0ea7a5cee7
shoki <ac.milan.22x36@gmail.com>
2018-01-11 12:03:09 +0900

    preperation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,6 +16,12 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+  end
+
   private
   def article_params
     params.require(:article).permit(:body, :title, :thum)
