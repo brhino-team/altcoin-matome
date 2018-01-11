a109ce7044f914f0d6dbb1d8c75117dcf0feccf7
shoki <ac.milan.22x36@gmail.com>
2018-01-11 12:39:24 +0900

    update fun

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i(show edit update)
+
   def new
     @article = Article.new
   end
@@ -13,16 +15,24 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @article.update(article_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:body, :title, :thum)
   end
