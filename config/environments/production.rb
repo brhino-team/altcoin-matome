bcbae79ccc74c27d2c9300a0fcef1bc6d3836bec
shoki <ac.milan.22x36@gmail.com>
2018-01-11 16:48:30 +0900

    トラッキングID

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  GA.tracker = "UA-112323260-1"
+  GA.tracker = "UA-112315297-1"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
