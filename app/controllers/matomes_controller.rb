2e42a8f983237c675d09b3a8833ed9480f663ee8
shoki <ac.milan.22x36@gmail.com>
2018-01-12 14:22:29 +0900

    render maincoin

--- app/controllers/matomes_controller.rb
+++ app/controllers/matomes_controller.rb
@@ -2,4 +2,8 @@ class MatomesController < ApplicationController
   def index
     @articles = Article.order("created_at DESC").page(params[:page]).per(10)
   end
+
+  def maincoin
+    @articles = Article.where(category_id: 1)
+  end
 end
