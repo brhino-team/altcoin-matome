ccc9aaa90c677a8a54fdc75a2cf45338c91b3e1d
shoki <ac.milan.22x36@gmail.com>
2018-01-12 10:40:52 +0900

    add category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180110122249) do
+ActiveRecord::Schema.define(version: 20180112013921) do
 
   create_table "articles", force: :cascade do |t|
-    t.string   "title",                  null: false
-    t.text     "body",                   null: false
-    t.integer  "pv",         default: 0
+    t.string   "title",                   null: false
+    t.text     "body",                    null: false
+    t.integer  "pv",          default: 0
     t.string   "thum"
-    t.string   "user_name",              null: false
+    t.string   "user_name",               null: false
     t.integer  "user_id"
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.integer  "category_id"
+    t.index ["category_id"], name: "index_articles_on_category_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "ckeditor_assets", force: :cascade do |t|
