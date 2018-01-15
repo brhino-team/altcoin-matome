1f6f697eadc0bece01dd7fca0204bd3dca1694b6
shoki <ac.milan.22x36@gmail.com>
2018-01-15 16:34:54 +0900

    write association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many :tags, through: :article_tags
+  has_many :article_tags, dependent: :destroy
   validates :title, :body, presence: true
   # mount_uploader :thum, ImageUploader
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,2 +1,5 @@
 class Tag < ApplicationRecord
+  validates :name, presence: true
+  has_many :tags, through: :articles_tags
+  has_many :articles_tags, dependent: :destroy
 end
