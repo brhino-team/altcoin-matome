27f9116e97e61f7d6587212a71a2b5214669dfdc
shoki <ac.milan.22x36@gmail.com>
2018-01-15 16:37:36 +0900

    タグ処理記述

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,21 @@ class Article < ApplicationRecord
   has_many :article_tags, dependent: :destroy
   validates :title, :body, presence: true
   # mount_uploader :thum, ImageUploader
+
+  def save_tags(tag)
+    current_tags = tags.pluck(:name) unless self.tags.nil?
+    old_tags = current_tags - tag
+    new_tags = tag - current_tags
+
+    # Destroy old taggings:
+    old_tags.each do |old_name|
+      tags.delete Tag.find_by(name: old_name)
+    end
+
+    # Create new taggings:
+    new_tags.each do |new_name|
+      article_category = Tag.find_or_create_by(name: new_name)
+      tags << article_tag
+    end
+  end
 end
