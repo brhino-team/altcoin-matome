a46aab394c0dee4f957647cbbd77e15d3257aab4
shoki <ac.milan.22x36@gmail.com>
2018-01-15 17:50:22 +0900

    fix

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: %i(show edit update destroy)
   before_action :redirect_root, only: %i(edit)
-  before_action :tag_listk, only: %i(create update)
+  before_action :tag_list, only: %i(create update)
 
   def new
     if user_signed_in?
